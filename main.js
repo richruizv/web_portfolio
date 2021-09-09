a3dae73802b56f11e4a4baf602b4d20a6d83bb6d
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2021-09-08 19:59:43 -0500

    wireframe of webpage

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener("visibilitychange", event => {
+    if (document.visibilityState == "visible") {
+        document.getElementById('title').innerHTML = '👋 Welcome back!';
+        console.log("tab is active")
+    } else {
+        document.getElementById('title').innerHTML ='💓 Comeback soon';
+      console.log("tab is inactive")
+    }
+  })
